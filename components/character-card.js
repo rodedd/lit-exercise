b90e6485081a1341142bbce0dd1e847093694458
Eduardo <96.rodedd@gmail.com>
2024-05-30 15:21:18 -0600

    adding like character functionality

--- components/character-card.js
+++ components/character-card.js
@@ -15,30 +15,84 @@ class CharacterCard extends LitElement {
       }
       
       .card h3 {
-        margin-top: 0;
+        margin: 0;
       }
 
       .card img {
         width: 100%;
         max-width: 250px;
       }
+
+      .title-container {
+        width: 100%;
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        margin-bottom: 10px;
+      }
+
+      .like-icon {
+        fill: none;
+        cursor: pointer;
+      }
+
+      .liked {
+        fill: red;
+      }
     `;
-  }
+  };
 
   static get properties() {
     return {
-      character: { type: Object }
+      character: { type: Object },
+      isCharacterLiked: { type: Boolean }
     };
+  };
+
+  constructor() {
+    super();
+    this.character = {};
+    this.isCharacterLiked = false;
   }
+
+  updated() {
+    this.isCharacterLiked = this.checkIsCharacterLiked(this.character.id);
+  };
+
+  toggleLikeCharacter(characterId) {
+    const likedCharacters = JSON.parse(localStorage.getItem('LIKED_CHARACTERS')) || [];
+    const characterIndex = likedCharacters.indexOf(characterId);
+
+    if(characterIndex !== -1) {
+      likedCharacters.splice(characterIndex, 1);
+      this.isCharacterLiked = false;
+    } else {
+      likedCharacters.push(characterId);
+      this.isCharacterLiked = true;
+    }
+    localStorage.setItem('LIKED_CHARACTERS', JSON.stringify(likedCharacters));
+  };
+
+  checkIsCharacterLiked(characterId) {
+    const likedCharacters = JSON.parse(localStorage.getItem('LIKED_CHARACTERS')) || [];
+    return likedCharacters.includes(characterId);
+  };
 
   render() {
     return html`
       <div class='card'>
-        <h3>${this.character.name}</h3>
-          <img src="${this.character.image}" alt="${this.character.name}" />
+        <div class='title-container'>
+          <h3>${this.character.name}</h3>
+          <div @click='${() => this.toggleLikeCharacter(this.character.id)}'>
+            <svg class='like-icon ${this.isCharacterLiked ? 'liked' : ''}' width="30px" height="30px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+              <path fill-rule="evenodd" clip-rule="evenodd" d="M12 6.00019C10.2006 3.90317 7.19377 3.2551 4.93923 5.17534C2.68468 7.09558 2.36727 10.3061 4.13778 12.5772C5.60984 14.4654 10.0648 18.4479 11.5249 19.7369C11.6882 19.8811 11.7699 19.9532 11.8652 19.9815C11.9483 20.0062 12.0393 20.0062 12.1225 19.9815C12.2178 19.9532 12.2994 19.8811 12.4628 19.7369C13.9229 18.4479 18.3778 14.4654 19.8499 12.5772C21.6204 10.3061 21.3417 7.07538 19.0484 5.17534C16.7551 3.2753 13.7994 3.90317 12 6.00019Z" stroke="#000000" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+            </svg>
+          </div>
+        </div>
+        <img src="${this.character.image}" alt="${this.character.name}" />
       </div>
     `;
-  }
-}
+  };
+};
 
 customElements.define('character-card', CharacterCard);
--- index.js
+++ index.js
@@ -43,6 +43,8 @@ class HomePage extends LitElement {
     this.apiUrl = 'https://rickandmortyapi.com/api/character';
     this.prevPageUrl = null;
     this.nextPageUrl = null;
+    this.hasLikedCharacters = false;
+    this.showingFavoriteCharacters = false;
   };
 
   async getCharacters(url) {
@@ -51,29 +53,39 @@ class HomePage extends LitElement {
     this.characters = data.results;
     this.prevPageUrl = data.info.prev ? data.info.prev : null;
     this.nextPageUrl = data.info.next ? data.info.next : null;
+    this.showingFavoriteCharacters = false;
   };
 
   firstUpdated() {
     this.getCharacters(this.apiUrl);
-  }
+  };
 
   changePage(value) {
     this.getCharacters(value === 'next' ? this.nextPageUrl : this.prevPageUrl);
     window.scrollTo({ top: 0, behavior: 'smooth' });
-  }
+  };
+
+  async getLikedCharacters() {
+    const likedCharacters = JSON.parse(localStorage.getItem('LIKED_CHARACTERS')) || [];
+    if(likedCharacters.length > 0) {
+      const response = await fetch(`${this.apiUrl}/${JSON.stringify(likedCharacters)}`);
+      const data = await response.json();
+      this.characters = data;
+      this.showingFavoriteCharacters = true;
+    }
+  };
 
   render() {
     return html`
       <div class='container'>
         <h1>Rick & Morty API w/ LitElement!</h1>
+        ${this.showingFavoriteCharacters ? html`<p><a href='#' @click='${() => this.getCharacters(this.apiUrl)}'>Show All Characters</a></p>`: html`<p><a href='#' @click='${this.getLikedCharacters}'>Show Liked Characters</a></p>`}
         <div class='card-container'>
-          ${
-            this.characters.map(character => html`<character-card .character='${character}'></character-card>`)
-          }
+          ${this.characters.map(character => html`<character-card .character='${character}'></character-card>`)}
         </div>
         <div class='card-buttons-container'>
-            <button @click='${() => this.changePage('prev')}' class='card-button' ?disabled='${!this.prevPageUrl}'>< Previous</button>
-            <button @click='${() => this.changePage('next')}' class='card-button' ?disabled='${!this.nextPageUrl}'>Next ></button>
+            <button @click='${() => this.changePage('prev')}' class='card-button' ?disabled='${!this.prevPageUrl || this.showingFavoriteCharacters}'>< Previous</button>
+            <button @click='${() => this.changePage('next')}' class='card-button' ?disabled='${!this.nextPageUrl || this.showingFavoriteCharacters}'>Next ></button>
         </div>
       </div>
     `;
